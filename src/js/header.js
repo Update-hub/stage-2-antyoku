14c5f441d97c43fb46b9fdc16784ea6b4d235331
antyoku <antyokunaoyasu@gmail.com>
2018-04-26 17:44:41 +0900

    開閉メニュー実装 #35

--- src/js/header.js
+++ src/js/header.js
@@ -1,20 +1,24 @@
 /** @param {int} target jump id offset */
 function jump(target) {
+  $('#menuOnOff').prop('checked', false);
   let speed = 400;
   $('body, html').animate({
       scrollTop: target,
     },
     speed, 'swing');
 };
-$('#js-feature').on('click', () => {
+$('.js-feature').on('click', () => {
   let targetPoint = $('#feature').offset().top;
   jump(targetPoint);
 });
-$('#js-popular').on('click', () => {
+$('.js-popular').on('click', () => {
   let targetPoint = $('#popular').offset().top;
   jump(targetPoint);
 });
-$('#js-media').on('click', () => {
+$('.js-media').on('click', () => {
   let targetPoint = $('#media').offset().top;
   jump(targetPoint);
+});
+$('.overlay').on('click', () => {
+  $('#menuOnOff').prop('checked', false);
 });
